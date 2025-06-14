2ddfe87ddb3bc46b5a8b88ac9fe83e11af900eef
FerTheOnePieceisReal <fernanda.cruz@ucsp.edu.pe>
2025-06-14 07:23:40 -0500

    Update perm2.cpp

--- perm2.cpp
+++ perm2.cpp
@@ -4,38 +4,42 @@
 constexpr double PI = 3.14159265358979323846;
 
 double Acircle(double* dims) {
-    return PI * std::pow(dims[0], 2);
+    return PI * std::pow(*(dims), 2);
 }
 double Asquare(double* dims) {
-    return std::pow(dims[0], 2);
+    return std::pow(*(dims), 2);
 }
 double Arectangle(double* dims) {
-    return dims[0] * dims[1];
+    return *(dims) * *(dims+1);
 }
 double Atriangle(double* dims) {
-    return (dims[0] * dims[1]) / 2;
+    return (*(dims) * *(dims+1)) / 2;
 }
 
 void ejecuta(double (*Areas[4])(double*), double parametros[4][2], char nombres[4][15]) {
-    for (int i = 0; i < 4; ++i) {
-        std::cout << "Area del " << nombres[i]
-                  << " con " << parametros[i][0] << " y " << parametros[i][1] << " :\n";
-        std::cout << Areas[i](parametros[i]) << std::endl;
+    for (
+        double (**pArea)(double*) = Areas;
+        pArea != Areas + 4;
+        ++pArea, ++parametros, ++nombres
+    ) {
+        std::cout << "Area del " << *nombres
+                  << " con " << *(*parametros) << " y " << *((*parametros)+1) << " :\n";
+        std::cout << (*pArea)(*parametros) << std::endl;
     }
 }
 
+
 int main() {
-    double (*Areas[4])(double*) = {Acircle, Asquare, Arectangle, Atriangle};
-    
-    // Para figuras que solo usan un parámetro, el segundo puede ser cero o ignorado
+    double (*Areas[4])(double*) = { Acircle, Asquare, Arectangle, Atriangle };
+
     double parametros[4][2] = {
         {5, 0},  // Círculo
         {4, 0},  // Cuadrado
         {6, 4},  // Rectángulo
         {3, 5}   // Triángulo
     };
-    
-    char nombres[4][15] = {"Circulo", "Cuadrado", "Rectangulo", "Triangulo"};
+
+    char nombres[4][15] = { "Circulo", "Cuadrado", "Rectangulo", "Triangulo" };
 
     ejecuta(Areas, parametros, nombres);
 
